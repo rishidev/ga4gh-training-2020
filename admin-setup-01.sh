2d7f3d39f61bf2eceaf9dbdc02a2a4640dfd9606
rishidev <rishidev@users.noreply.github.com>
2020-02-09 21:17:15 +0000

    Update admin-setup-01.sh

--- admin-setup-01.sh
+++ admin-setup-01.sh
@@ -1,5 +1,14 @@
 sudo apt install git
+sudo apt install python-pip
+python -m pip install --upgrade pip setuptools wheel
+
 sudo useradd train
 # set password to ga4gh2020 for this instance
 sudo passwd train
+
+mkdir sw_setups
+cd sw_setups
+
+# htsget
+git clone https://gitlab.com/genomicsengland/htsget/gel-htsget.git
 
